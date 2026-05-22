95bb94203a4ac2a61cc19d43bb049dea4a0a0aea
gitcrxr-byte <gitcrxr@gmail.com>
2026-05-22 19:59:20 -0400

    feat: add src/App.jsx as main component

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <>
+      <h1>Catálogo de Asensores</h1>
+      <p>¡Bienvenido a tu aplicación React con Vite!</p>
+    </>
+  );
+}
+
+export default App;
